01028215d36ba19aa79c94512fd943b4cf0bf133
Bluey95 <susanwere15@gmail.com>
2018-08-16 11:27:59 +0300

    changes to Api

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from config import Config
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
 app.config.from_object(Config)
+api = Api(app)
+
 
 from app import routes
+
