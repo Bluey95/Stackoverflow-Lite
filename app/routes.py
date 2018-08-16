346c354e6c14584aaa7a24b0603bd7fe48a41ef9
Bluey95 <susanwere15@gmail.com>
2018-08-16 14:58:54 +0300

    [ch #159822184] Set up application server

--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,6 @@ from flask import Flask, request, flash, redirect, url_for, jsonify
 from app import app
 
 @app.route('/')
-
-@app.route('/index')
 def index():
     
     return "Hello its working."
