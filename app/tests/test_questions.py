ed5dba36524c6fcc24076961ae51eba85a6ec68b
Bluey95 <susanwere15@gmail.com>
2018-08-19 17:55:33 +0300

    requirments

--- app/tests/test_questions.py
+++ app/tests/test_questions.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, flash, redirect, url_for, jsonify
 import unittest
-from .questions.models import Question
+from models import Question
 
 
 class Question_tests(unittest.TestCase):
