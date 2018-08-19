c4f5bf05522c7ff6fcb9483a3ba9f4bbf062c942
Bluey95 <susanwere15@gmail.com>
2018-08-19 13:09:16 +0300

    [ft #159873214] API endpoint to login a user

--- app/users/views.py
+++ app/users/views.py
@@ -18,3 +18,17 @@ def reg():
                     return "Password too short"
             userObject.create(username, password)
             return jsonify({"message":"Dear " + username + " you have been succesfully registered."})
+
+@user_api.route('/login', methods=["POST"])
+def login():
+    """ Method to login user """
+    user_details = request.get_json()
+    username = user_details['username']
+    password = user_details['password']
+    res = userObject.login(username, password)
+    if res == "successful":
+        for user in userObject.user_list:
+            if user['username'] == username and user['password'] == password:
+                return jsonify(response ="login successful"), 200
+    return jsonify({"message" : res })
+    
