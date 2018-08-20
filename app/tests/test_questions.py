44ff2407155e5e49aecd2dccf0673e02c8a6d038
Bluey95 <susanwere15@gmail.com>
2018-08-20 19:02:35 +0300

    tests

--- app/tests/test_questions.py
+++ app/tests/test_questions.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, flash, redirect, url_for, jsonify
 import unittest
+import json
 from ..questions.models import Question
-
 
 class Question_tests(unittest.TestCase):
     def setUp(self):
@@ -12,27 +12,38 @@ class Question_tests(unittest.TestCase):
         """ clear up global object after each test """
         del self.question
 
+    def create(self):
+        """ create questions. """
+        res = self.question.create(title = "blue", body = "Is green better?")
+        return res
+
     def test_post_question(self):
         """ test that can create a question """
-        response = self.question.create(title = "blue", body = "Is green better?")
+        response = self.create()
         self.assertEqual(response[0]["title"], "blue")
 
     def test_get_question(self):
         """ test that can get a question """
-        self.question.get_question()
-        self.assertEqual("blue", "blue")
+        self.create() # Create question
+        res = self.question.get_question()
+        self.assertEqual(len(res), 1)
 
-    def test_get_specific_question(self):
-        """ test that can get a specific question """
-        app = Flask(__name__)
-        with app.app_context():
-            self.question.get_specific_question(id = "1")
-            self.assertEqual("1", "1")
+    # def test_get_specific_question(self):
+    #     """ test that can get a specific question """
+    #     app = Flask(__name__)
+    #     with app.app_context():
+    #         self.create()
+    #         res = self.question.filter_by_id(1)
+    #         r = json.loads(res)
+    #         self.assertEqual(r['title'], "blue")
 
-    def test_post_answer(self):
-        """ test that can add a question """
-        self.question.add_answer(qid = "1", comment = "rachel", upvote="0", downvote="0")
-        self.assertEqual("rachel", "rachel")
+    # def test_post_answer(self):
+    #     """ test that can add a question """
+    #     app = Flask(__name__)
+    #     with app.app_context():
+    #         self.create()
+    #         r = self.question.add_answer(1, "rachel", 0, 0)
+    #         self.assertEqual(r.get('comment'), 1)
 
 
        
