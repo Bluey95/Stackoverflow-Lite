3f9b2e196ec67ca84027a4ad3682142ecadbc2f7
Bluey95 <susanwere15@gmail.com>
2018-08-22 12:49:39 +0300

    osenviron
    [#159918651]

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 import os
 from config import app_config
 # third-party imports
-from flask import Flask
+from flask import Flask, redirect
 
 # local imports
 from config import app_config
@@ -17,5 +17,10 @@ def create_app(config_name):
 
     from .users import user_api as api_blueprint
     app.register_blueprint(api_blueprint, url_prefix='/api/v1/auth')
+
+    
+    @home_blueprint.route('/', methods=['GET', 'POST'])
+    def home():
+        return redirect("/api/v1/questions", code=302)
 
     return app
