ea07aea84ff8c0f80daf02ae3b5752ef3500b34b
Bluey95 <susanwere15@gmail.com>
2018-08-24 05:25:31 +0300

    [ft #
    [#159972295]

--- app/questions/models.py
+++ app/questions/models.py
@@ -58,8 +58,11 @@ class Question(object):
         """ Serialize tuple into dictionary """
         cur.execute("SELECT * FROM questions WHERE id = %s;", (id,))
         question = cur.fetchone()
+        ans = Answer()
+        answers = ans.fetch_answers_by_question_id(id)
+        print(answers)
         if question:
-            return self.question_serialiser(question)
+            return jsonify({"Question":self.question_serialiser(question), "Answers":answers})
         return False
     
     def is_owner(self, question_id, userid):
@@ -83,3 +86,54 @@ class Question(object):
             "DELETE FROM questions WHERE id=%s", (question_id, ))
         self.save()
         return "Deleted Successfully"
+
+class Answer(object):
+    def __init__(self, body=None, question_id=None): 
+        super(Answer, self).__init__()
+        self.body = body
+        self.question_id = question_id
+
+    def save(self):
+        conn.commit()
+
+    def create(self):
+        """Create Answers"""
+        answered_by = g.username
+        user_id = g.userid
+        question_id = self.question_id
+        cur.execute(
+                """
+                INSERT INTO answers (body, answered_by, user_id, question_id)
+                VALUES (%s, %s, %s, %s) RETURNING id;
+                """,
+                (self.body, answered_by, user_id, self.question_id))
+        """fetch the new Answer, pick the id, and assign to questionid"""
+        questionid = cur.fetchone()[0]
+        """save Answer"""
+        self.save()
+        question = Question()
+        return question.fetch_question_by_id(question_id), 201
+
+    def fetch_answers_by_question_id(self, id):
+        """ Serialize tuple into dictionary """
+        cur.execute("SELECT * FROM answers WHERE question_id = %s;", (id,))
+        answers_tuple = cur.fetchall()
+        answers = []
+
+        if len(answers_tuple) > 0:
+            for answer in answers_tuple:
+                """append answers after serializing to the list"""
+                answers.append(self.answers_serialiser(answer))
+            return answers
+        return answers_tuple
+
+    def answers_serialiser(self, answer):
+        """ Serialize tuple into dictionary """
+        answer_details = dict(
+            id=answer[0],
+            question_id=answer[3],
+            body=answer[1],
+            answered_by=answer[2],
+            user_id=answer[4]
+        )
+        return answer_details
