69d9ff43077b793ee11405a817ecb2e65494c21c
Bluey95 <susanwere15@gmail.com>
2018-08-24 07:50:43 +0300

    update connection

--- connect.py
+++ connect.py
@@ -1,14 +1,10 @@
 import os
 import psycopg2
 
-print(os.getenv('dbname'))
-if os.getenv('FLASK_CONFIG') == "development":
-    conn = psycopg2.connect(dbname=os.getenv('dbname'), user=os.getenv('user'), password=os.getenv('password'))
-elif os.getenv('FLASK_CONFIG') == "testing":
-    conn = psycopg2.connect(dbname='test', user=os.getenv('user'), password=os.getenv('password'))
+conn = psycopg2.connect(dbname=os.getenv('dbname'), user=os.getenv('user'), password=os.getenv('password'))
+
 
 try:
-    # Connect to our db
     conn
 
 except (Exception, psycopg2.DatabaseError) as error:
