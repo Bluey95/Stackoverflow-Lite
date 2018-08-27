149347fb69ba32a52e102decf109af1346c1bb85
Bluey95 <susanwere15@gmail.com>
2018-08-27 15:53:12 +0300

    jwt

--- app/users/views.py
+++ app/users/views.py
@@ -79,7 +79,7 @@ def login():
         user = userObject.get_user_by_username(user_details['username'])
         if user and userObject.verify_password(user_details['password'], user['password']):
             auth_token = jwt_obj.generate_auth_token(user["id"])
-            return jsonify({"user": user, "message": "Login Successfull.", "Auth":auth_token}), 201
+            return jsonify({"user": user, "message": "Login Successfull.", "Access_token":auth_token}), 201
             
         else:
             response = {'message': 'invalid username or password, Please try again'}
