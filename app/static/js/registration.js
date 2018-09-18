e793b3f5090c1b806e54b546569f625303da80a0
Bluey95 <susanwere15@gmail.com>
2018-09-18 20:52:26 +0300

    fetch endpoints for registration

---
+++ app/static/js/registration.js
@@ -0,0 +1,16 @@
+let username = document.getElementById('Username').value;
+let email = document.getElementById('Email').value;
+let password = document.getElementById('Password').value;
+let confirmPassword = document.getElementById('ConfirmPassword').value;
+
+fetch('https://stackoverflowlitev3.herokuapp.com/api/v2/auth/registration', {
+    method: 'POST',
+    body:JSON.stringify({
+        username:username, 
+        email:email, 
+        password:password, 
+        confirmPassword:confirmPassword})
+}).then(function(response) {
+    return response.json();
+})
+     
