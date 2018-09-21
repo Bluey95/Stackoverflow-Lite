d6219e6f2bc7c27e5a6d89f69e50f1e04168f244
Bluey95 <susanwere15@gmail.com>
2018-09-21 11:45:30 +0300

    [160584151] user registration

--- app/static/js/registration.js
+++ app/static/js/registration.js
@@ -1,35 +1,26 @@
-function validateForm() {
-    var name = document.forms["myForm"]["Username"].value;
-    if (name.length < 3) {
-        alert("username must be more than 3 characters");
-        return false;
-    }else if (!(/^[A-Za-z\s]+$/.test(name))){
-        alert("Your username should only contain letters");
-        return false;
-    } else {
-        return true;
+function submitForm(){
+    let username = "cheruto"
+    let email = "cheruto@gmail.com"
+    let password = "Cheruto123"
+    let confirmpass = "Cheruto123"
+    
+    var http = new XMLHttpRequest();
+    var url = 'https://stackoverflowlitev3.herokuapp.com/api/v2/auth/registration';
+    var params = JSON.stringify({
+        "username" : username, 
+        "email" : email, 
+        "password" : password, 
+        "confirmpass" : confirmpass
+    });
+    http.open('POST', url, true);
+    
+    //Send the proper header information along with the request
+    http.setRequestHeader('Content-type', 'application/json');
+    
+    http.onreadystatechange = function() {//Call a function when the state changes.
+        if(http.readyState == 4 && http.status == 200) {
+            alert(http.responseText);
+        }
     }
-} 
-
-let username = document.getElementById('Username').value;
-let email = document.getElementById('Email').value;
-let password = document.getElementById('Password').value;
-let confirmPassword = document.getElementById('ConfirmPassword').value;
-
-fetch('https://stackoverflowlitev3.herokuapp.com/api/v2/auth/registration', {
-    method: 'POST',
-    mode: 'cors', 
-	redirect: 'follow',
-	headers: new Headers({
-		'Content-Type': 'application/json'
-	}),
-    body:JSON.stringify({
-        username:username, 
-        email:email, 
-        password:password, 
-        confirmpass:confirmPassword})
-}).then(function(response) {
-    return response.json();
-}).then(function(body) {
-    console.log(body);
-})
+    http.send(params);
+    }
