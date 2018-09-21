01dd48775244a07bd3e2bf058e94c1ff4a962b52
Bluey95 <susanwere15@gmail.com>
2018-09-21 12:50:21 +0300

    [#160681938] implement CORS

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ from flask_cors import CORS, cross_origin
 
 config_name = os.getenv('FLASK_CONFIG')
 app = create_app(config_name)
-cors = CORS(app, resorces={r'/api/v2/*': {"origins": '*'}})
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5050))
