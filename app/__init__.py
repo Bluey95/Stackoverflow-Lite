3a7679d4b784a945b2529765202db9fb3d92a2c7
Bluey95 <susanwere15@gmail.com>
2018-09-21 12:58:34 +0300

    [#160681938] implement CORS

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from config import app_config
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
+    cors = CORS(app, resorces={r'/api/v2/*': {"origins": '*'}})
     app.url_map.strict_slashes = False
     # print(app_config.get('FLASK_CONFIG'))
     # print('*'*123)
