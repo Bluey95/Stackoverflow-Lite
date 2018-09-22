38d64a9bf7a770c5401e189dd00d3602c78e1bbc
Bluey95 <susanwere15@gmail.com>
2018-09-23 01:02:15 +0300

    [#160703933] postQuestions

--- app/static/js/ask.js
+++ app/static/js/ask.js
@@ -1,53 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var button = document.getElementById("submit");
-    button.onclick = function(){
-        var questionTitle = document.getElementById("questionTitle").value;
-        var questionBody = document.getElementById("questionBody").value;
-        var getTOken = window.sessionStorage.getItem('token');
-        var token = "Bearer " + getTOken;
-        alert(token)
-
-        p = {
-            title:questionTitle,
-            body:questionBody
-        }
-        console.log(p)
-
-        fetch('https://stackoverflowlitev3.herokuapp.com/api/v2/questions', {
-        method: 'POST',
-        mode: 'cors', 
-        crossdomain: true,
-        redirect: 'follow',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': token
-        },
-        body: p
-        }).then(function(response) {
-        if (response.status == 201){
-            response.json().then(data => example(data));
-            window.location.replace("index.html")
-        }else if (response.status == 400 || response.status == 422){
-            response.json().then(
-                data => 
-                { var arr = [];
-
-                for (var key in data) {
-                    if (data.hasOwnProperty(key)) {
-                        arr.push( [ key, data[key] ] );
-                    }
-                }alert(data[key]); window.location.reload(true);});
-        }
-        else{
-        //failed
-        response.json().then(data => console.log("Failed: ", data));
-        }
-        }).catch(err => console.log(err));
-        function example(data){
-            //execute some statements
-            console.log(JSON.stringify(data));
-        }
-        return false;
-    }
-}) 
-
+var request = new XMLHttpRequest();
+console.log("starting here");
+request.open('POST', 'https://stackoverflowlitev3.herokuapp.com/api/v2/questions', true);
+request.withCredentials = true;
+console.log(".............");
+console.log("then here");
+token = window.sessionStorage.getItem("token");
+console.log(token)
+console.log("and then here.........");
+request.setRequestHeader('Authorization', 'Basic [base64 encoded password here]' );
+request.onload = function () {
+}
+request.send();
