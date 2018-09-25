a4d75f27ef62dba6b976cc01459189f16d52e53c
Bluey95 <susanwere15@gmail.com>
2018-09-25 23:28:00 +0300

    Authorize User

--- app/users/__init__.py
+++ app/users/__init__.py
@@ -1,7 +1,5 @@
 from flask import Blueprint
-from flask_cors import CORS
 
 user_api = Blueprint('user_api', __name__)
-user_api
 
 from . import views
--- run.py
+++ run.py
@@ -1,11 +1,9 @@
 import os
 
 from app import create_app
-from flask_cors import CORS
 
 config_name = os.getenv('FLASK_CONFIG')
 app = create_app(config_name)
-CORS(app)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5050))
